3e64aefbda05771f8f8d3c4893f2473c671f886c
Matthew Worthington <mattjames8723@gmail.com>
2025-02-03 21:17:43 -0500

    Fix: debugging of echo statements

--- homework2.php
+++ homework2.php
@@ -1,23 +1,25 @@
 <!DOCTYPE html>
-    <head>
-        <meta charset ="UTF=8">
-        <title>My first PHP Website</title>
-    </head>
-    <body>
-        <?php
-            $time = date("H");//sets time to the hor in a 24 hour time format
-            $timezone = date("e");//sets timezone
-            echo "The current time since the Unix epoch is : " + time();
-            echo "The current time and date is: " + date("D M j G:i:s T Y");
-            if ($time < 12) {
-                echo "Good Morning!";
-            } else if ($time >= 12 || $time < 17) {
-                echo "Good Afternoon!";
-            } else if ($time >= 17 || $time < 20) {
-                echo "Good Evening!";
-            } else {
-                echo "Good Night!";
-            }
-        ?>
-    </body>        
+<head>
+    <title>My first PHP Site</title>
+    <meta charset="UTF-8">
+</head>
+<body>
+<?php
+    date_default_timezone_set("America/New_York");//sets timezone
+    $time = date("H");//sets time to the hour in a 24 hour time format
+    echo "The current time since the Unix epoch is : ";
+    echo time();
+    echo " The current time and date is: "; 
+    echo date(" D M j G:i:s T Y");
+    if ($time < 12) {
+        echo " Good Morning!";
+    } else if ($time >= 12 && $time < 17) {
+        echo " Good Afternoon!";
+    } else if ($time >= 17 && $time < 20) {
+        echo " Good Evening!";
+    } else {
+        echo " Good Night!";
+    }
+?>
+</body>
 </html>
