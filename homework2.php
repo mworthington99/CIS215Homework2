d32de7b2a77dda26c0722e7aa2b336bb9d1e6fc2
Matthew Worthington <mattjames8723@gmail.com>
2025-02-03 19:33:34 -0500

    Feat: added time sensitive user messages

--- homework2.php
+++ homework2.php
@@ -5,9 +5,19 @@
     </head>
     <body>
         <?php
-            $time = date("H");
-            $timezone = date("e");
-            
+            $time = date("H");//sets time to the hor in a 24 hour time format
+            $timezone = date("e");//sets timezone
+            echo "The current time since the Unix epoch is : " + time();
+            echo "The current time and date is: " + date("D M j G:i:s T Y");
+            if ($time < 12) {
+                echo "Good Morning!";
+            } else if ($time >= 12 || $time < 17) {
+                echo "Good Afternoon!";
+            } else if ($time >= 17 || $time < 20) {
+                echo "Good Evening!";
+            } else {
+                echo "Good Night!";
+            }
         ?>
     </body>        
 </html>
